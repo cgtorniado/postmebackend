50653b5e2e8bfed6c761c0b90a52c9398aad3c42
cgtorniado <cleo.torniado@gmail.com>
2022-05-17 16:29:05 +0800

    hmm

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.post('/search', (req,res) => {
     const {search} = req.body
    
     db.query(
-        "select * from register where first_name = ? or last_name=?",[search,search], 
+        "select * from register where firstName = ? or lastName=?",[search,search], 
         (err,result)=> {
             if(err){
                 return console.log(err.message)
