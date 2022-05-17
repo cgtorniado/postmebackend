e9e8cbd8a8fdfcafa9a6ec437cf7619fcdc10cf8
cgtorniado <cleo.torniado@gmail.com>
2022-05-17 13:47:29 +0800

    plz

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use("/public", express.static(
 //multer
 const storage = multer.diskStorage({
     destination:  (req,file,callback)=> {
-        callback(null,path.join(__dirname,"../public/images"))
+        callback(null,path.join(__dirname,"./public/images"))
     },
     filename:(req,file,callback)=> {
         callback(null,file.fieldname+"-"+Date.now()+path.extname(file.originalname))
