dc5de975c0c670e32cde7695f960789f7c771d35
cgtorniado <cleo.torniado@gmail.com>
2022-05-18 01:39:02 +0800

    other posts

--- index.js
+++ index.js
@@ -203,7 +203,7 @@ app.post('/newpostother', async (req,res)=> {
 
 app.post('/postfeed', async (req,res)=> {
     const {userid} = req.body
-    db.query("select posts.postid, posts.userid,  posts.content, posts.date_created, posts.date_updated, register.firstName, register.lastName, register.username from posts inner join register on posts.userid = register.userid where posts.wallid=? order by posts.postid desc",[userid] , 
+    db.query("select posts.postid, posts.userid, posts.wallid, posts.content, posts.date_created, posts.date_updated, register.firstName, register.lastName, register.username from posts inner join register on posts.userid = register.userid where posts.wallid=? order by posts.postid desc",[userid] , 
     (err,result) => {   
         if(err){
             console.log(err.message);
