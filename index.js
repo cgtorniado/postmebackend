da786b0120227a8c7e8c8b0c229c852bd43bf460
cgtorniado <cleo.torniado@gmail.com>
2022-05-19 17:46:59 +0800

    notif feed

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ app.post('/notiffeed',  (req,res)=> {
     from notifications as notif
     inner join register as receiver on notif.notifreceiverid = receiver.userid
     inner join register as sender on notif.notifsenderid = sender.userid
-    where notif.notifreceiverid=?`,[userid] , 
+    where receiver.userid=?`,[userid] , 
     (err,result) => {   
         if(err){
             console.log(err.message);
